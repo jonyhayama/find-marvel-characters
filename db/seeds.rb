8c13ed1b513689f8f84536f5607a35fdf38dcea4
Jony Hayama <jonathan.hayama@gmail.com>
2022-08-06 07:57:37 -0300

    🌱 Add seed data for Marvel Characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+characters = []
+
+CSV.read('db/marvel-characters.csv').each do |character|
+  characters << {
+    external_id: character[0].strip,
+    name: character[1].strip,
+    thumbnail_url: character[2].strip,
+  }
+end
+
+Character.upsert_all(characters, unique_by: :external_id)
