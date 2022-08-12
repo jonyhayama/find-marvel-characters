70cd9618b0944a5e0a5d55769dfc25961d99b474
Jony Hayama <jonathan.hayama@gmail.com>
2022-08-12 16:59:21 -0300

    ✨ Improve search results

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,6 +1,6 @@
 class Api::CharactersController < ApplicationController
   def index
-    query = params[:query].downcase
+    query = params[:query].downcase.gsub(/ /, '%')
     @characters = Character.where('lower(name) LIKE ?', "%#{query}%").order_by_name_similarity(query).limit(10)
 
     expires_in 10.minutes
